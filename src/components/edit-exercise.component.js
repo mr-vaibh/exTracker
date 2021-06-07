b5c35860249edcb2de453ce377c6889c10a1aa83
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2021-06-07 13:16:08 +0530

    Update edit-exercise.component.js

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -23,7 +23,7 @@ export default class EditExercise extends Component {
 	}
 
 	componentDidMount() {
-		axios.get('http://localhost:5000/exercises/' + this.props.match.params.id)
+		axios.get('/exercises/' + this.props.match.params.id)
 			.then(res => {
 				this.setState({
 					username: res.data.username,
@@ -33,7 +33,7 @@ export default class EditExercise extends Component {
 				});
 			})
 			.catch(err => console.log('Frontend Error: ' + err))
-		axios.get('http://localhost:5000/users')
+		axios.get('/users')
 			.then(res => {
 				if (res.data.length > 0) {
 					this.setState({
@@ -79,7 +79,7 @@ export default class EditExercise extends Component {
 		}
 
 		console.log(exercise);
-		axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+		axios.post('/exercises/update/' + this.props.match.params.id, exercise)
 			.then(res => console.log(res.data))
 			.catch(err => console.log('Error in frontend:\n' + err))
 
